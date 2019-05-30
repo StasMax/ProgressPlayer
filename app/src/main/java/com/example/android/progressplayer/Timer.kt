bb88d3ef99f2baf25094e15bb4ef6782ded53d2c
StasMax <maximovich@live.ru>
2019-05-30 08:44:07 +0300

    del button pause, add track time

---
+++ app/src/main/java/com/example/android/progressplayer/Timer.kt
@@ -0,0 +1,13 @@
+package com.example.android.progressplayer
+
+import java.util.*
+
+fun runTimer(seconds: Int): String {
+    val minutes = (seconds % 3600) / 60
+    val secs = seconds % 60
+    return String.format(
+        Locale.getDefault(),
+        "%02d:%02d", minutes, secs
+    )
+}
+
--- app/src/main/java/com/example/android/progressplayer/ui/MainActivity.kt
+++ app/src/main/java/com/example/android/progressplayer/ui/MainActivity.kt
@@ -1,9 +1,14 @@
 package com.example.android.progressplayer.ui
 
+import android.content.Context
+import android.graphics.Point
 import android.os.Bundle
+import android.os.SystemClock
+import android.util.Log
 import android.view.GestureDetector
 import android.view.MotionEvent
 import android.view.View
+import android.widget.Chronometer
 import android.widget.SeekBar
 import com.arellomobile.mvp.MvpAppCompatActivity
 import com.arellomobile.mvp.presenter.InjectPresenter
@@ -11,6 +16,7 @@ import com.example.android.progressplayer.LEFT
 import com.example.android.progressplayer.R
 import com.example.android.progressplayer.RIGHT
 import com.example.android.progressplayer.presenter.MainPresenter
+import com.example.android.progressplayer.runTimer
 import com.example.android.progressplayer.view.MainView
 import kotlinx.android.synthetic.main.activity_main.*
 
@@ -18,6 +24,7 @@ class MainActivity : MvpAppCompatActivity(), MainView {
 
     @InjectPresenter
     lateinit var mainPresenter: MainPresenter
+    private var isPause = true
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -38,6 +45,7 @@ class MainActivity : MvpAppCompatActivity(), MainView {
             }
 
             override fun onProgressChanged(seekBar: SeekBar?, progress: Int, fromUser: Boolean) {
+                trackTimer.text = runTimer(progress)
             }
         })
     }
@@ -53,10 +61,6 @@ class MainActivity : MvpAppCompatActivity(), MainView {
 
         rightFrame.setOnTouchListener { _, event -> gestureDoubleTapR.onTouchEvent(event) }
 
-        rightFrame.setOnLongClickListener {
-            mainPresenter.initLongPress(RIGHT)
-            true
-        }
 
         val gestureDoubleTapL = GestureDetector(this, object : GestureDetector.SimpleOnGestureListener() {
             override fun onDoubleTap(e: MotionEvent?): Boolean {
@@ -67,10 +71,7 @@ class MainActivity : MvpAppCompatActivity(), MainView {
 
         leftFrame.setOnTouchListener { _, event -> gestureDoubleTapL.onTouchEvent(event) }
 
-        leftFrame.setOnLongClickListener {
-            mainPresenter.initLongPress(LEFT)
-            true
-        }
+
     }
 
     override fun moveProgress(progress: Int) {
@@ -97,9 +98,16 @@ class MainActivity : MvpAppCompatActivity(), MainView {
 
     private fun initButtons() {
         buttonNext.setOnClickListener { mainPresenter.initNextTrack() }
-        buttonPause.setOnClickListener { mainPresenter.initPauseProgress() }
         buttonPreview.setOnClickListener { mainPresenter.initPreviewTrack() }
-        buttonStart.setOnClickListener { mainPresenter.initStartProgress(seekBar.progress.toLong()) }
+        buttonStart.setOnClickListener {
+            isPause = if (isPause) {
+                mainPresenter.initStartProgress(seekBar.progress.toLong())
+                false
+            } else {
+                mainPresenter.initPauseProgress()
+                true
+            }
+        }
         buttonStop.setOnClickListener { mainPresenter.initStopProgress() }
     }
 }
